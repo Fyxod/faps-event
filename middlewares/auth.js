3477246a0d75a6da611984784dbce3a607e8b3de
Parth Katiyar <parthdev0101@gmail.com>
2024-08-31 14:44:53 +0530

    debugging

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,6 +4,7 @@ export default function checkAuth(req, res, next) {
 
     // search for token if and only if cookies exist
     const token = req.headers['authorization']; 
+    console.log(req.headers);
     console.log(token);
     if (!token) {
         return next();
